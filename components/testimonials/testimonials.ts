1178353c64bc4033579f9bc87888d66caafc593e
Satvickk <lenovo37819@gmail.com>
2024-09-11 15:16:54 +0530

    Completed Updates

--- components/testimonials/testimonials.ts
+++ components/testimonials/testimonials.ts
@@ -10,29 +10,29 @@ export interface TestimonialType {
 export const testimonials: TestimonialType[] = [
   {
     id: 1,
-    name: "Amy Johnson",
+    name: "Dia Malhotra",
     role: "satisfied salon owner",
     quote:
       "Smart Salon Bot has transformed how I manage my salon. No more worries about staff scheduling or inventory!",
     rating: 4.9,
-    imageSrc: "/amy-johnson.jpg",
+    imageSrc: "/dia-malhotra.jpg",
   },
   {
     id: 2,
-    name: "Bruce Williams",
+    name: "Anurag Vishwaas",
     role: "salon manager",
     quote:
       "Implementing Smart Salon Bot was the best decision for our business. Our sales have increased significantly!",
     rating: 5,
-    imageSrc: "/bruce-williams.jpg",
+    imageSrc: "/anurag-vishwaas.jpg",
   },
   {
     id: 3,
-    name: "Emma Lee",
+    name: "Sunita Sharma",
     role: "regular salon client",
     quote:
       "Managing bookings and customer interactions has never been so easy. Thank you, Smart Salon Bot!",
     rating: 4.8,
-    imageSrc: "/emma-lee.jpg",
+    imageSrc: "/sunita-sharma.jpg",
   },
 ];
